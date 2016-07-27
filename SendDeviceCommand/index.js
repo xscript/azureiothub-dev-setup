22d958be7b859c1319837d671252d83dbf34e645
Joe Binder <binder.joe@gmail.com>
2016-07-27 13:12:35 -0700

    Fix bug when specifying the device target.

--- SendDeviceCommand/index.js
+++ SendDeviceCommand/index.js
@@ -7,43 +7,54 @@ var Message = require('azure-iot-common').Message;
 var connectionString = process.env.AzureIoTHubConnectionString;
 
 module.exports = function (context, input) {
-    var client = Client.fromConnectionString(connectionString);
-    var targetDevice = input.deviceId;
-    var message = new Message('blink');
-    message.ack = 'full';
-    message.messageId = "SomeMessageId";
-    
-    client.open(function (err) {
-        client.getFeedbackReceiver(receiveFeedback);
-        if (err) {
-          context.error('Could not connect: ' + err.message);
-        } else {
-            context.log(input);
-            context.log(message);
-            client.send(input, message, function(err){
-                context.log('ERROR: ' + err);
-            });
-        }
-    });
-    // Helper function to print results in the console
-function printResultFor(op) {
-  return function printResult(err, res) {
-    if (err) {
-      context.log(op + ' error: ' + err.toString());
-    } else {
-      context.log(op + ' status: ' + res.constructor.name);
-    }
-  };
-}
+  var client = Client.fromConnectionString(connectionString);
+  var targetDevice = input.deviceId;
+  var message = new Message('blink');
+  message.ack = 'full';
+  message.messageId = generateMessageId();;
 
-function receiveFeedback(err, receiver){
-  receiver.on('message', function (msg) {
-    context.log('Feedback message:')
-    context.log(msg.getData().toString('utf-8'));
+  client.open(function (err) {
+    client.getFeedbackReceiver(receiveFeedback);
+    if (err) {
+      context.error('Could not connect: ' + err.message);
+    } else {
+      context.log(message);
+      client.send(targetDevice, message, function (err) {
+        if (err) {
+          context.log('ERROR: ' + err);
+        }
+      });
+    }
   });
-}
-    context.done();
-    
+  // Helper function to print results in the console
+  function printResultFor(op) {
+    return function printResult(err, res) {
+      if (err) {
+        context.log(op + ' error: ' + err.toString());
+      } else {
+        context.log(op + ' status: ' + res.constructor.name);
+      }
+    };
+  }
+
+  function generateMessageId() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+      s4() + '-' + s4() + s4() + s4();
+  }
+
+  function receiveFeedback(err, receiver) {
+    receiver.on('message', function (msg) {
+      context.log('Feedback message:')
+      context.log(msg.getData().toString('utf-8'));
+    });
+  }
+  context.done();
+
 }
 
 
