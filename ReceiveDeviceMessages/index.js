3fe320c26b8659cdf2650b9be2c85f85a69ddbd4
Joe Binder <binder.joe@gmail.com>
2016-07-27 13:11:45 -0700

    Make the output format generic--it now just prints out the content of the message as-is

--- ReceiveDeviceMessages/index.js
+++ ReceiveDeviceMessages/index.js
@@ -1,12 +1,11 @@
-// This function is triggered each time a message is revieved in the IoTHub.  The device Id and temperature values
-// are persisted in an Azure Storage Table
+// This function is triggered each time a message is revieved in the IoTHub.  
+// The message payload is persisted in an Azure Storage Table
 var moment = require('moment');
 
-module.exports = function (context, myEventHubTrigger) {
-    context.bindings.outputTable = myEventHubTrigger;
-    context.bindings.outputTable.partitionKey = myEventHubTrigger.deviceId + ':' + moment().format('YYYY_MM_DD');
+module.exports = function (context, iotHubMessage) {
+    context.log('Message received: ' + JSON.stringify(iotHubMessage));
+    context.bindings.outputTable = iotHubMessage;
+    context.bindings.outputTable.partitionKey = moment().format('YYYY_MM_DD');
     context.bindings.outputTable.rowKey = moment().format('hh:mm:ss:SS');
-    context.log('Message received: ' + myEventHubTrigger.deviceId + ' ' + myEventHubTrigger.temperature);
     context.done();
 };
-
